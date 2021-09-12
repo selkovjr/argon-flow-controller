e0381c6c3d839c28daea9a4c2a8856b76b5e1d77
Gene Selkov <selkovjr@gmail.com>
2021-09-11 23:40:06 -0500

    separated interface and implementation in Display

--- flow_controller/Display.h
+++ flow_controller/Display.h
@@ -1,14 +1,13 @@
+#pragma once
+
 #include "SPI.h"              // Includes library for SPI communication of display
 #include "Adafruit_GFX.h"     // Includes core graphics library
 #include "Adafruit_SSD1351.h" // Includes hardware specific library
 #include <Fonts/FreeSansOblique12pt7b.h>
 
-
 #include "face-icon.c"
-
 extern const uint16_t face_icon[];
 
-// Screen dimensions
 #define SCREEN_WIDTH 128 //pixel width
 #define SCREEN_HEIGHT 128 //pixel height
 
@@ -22,7 +21,6 @@ extern const uint16_t face_icon[];
 #define DC_PIN   10 // master-in slave-out pin (7 in Uno)
 #define RST_PIN   8 // reset pin (same as in Uno)
 
-// Colour definitions
 #define BLACK           0x0000
 #define BLUE            0x001F
 #define RED             0xF800
@@ -36,26 +34,6 @@ extern const uint16_t face_icon[];
 
 
 namespace Display {
-  // Software SPI mode (slow)
-  // Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
-
-  // Hardware SPI using SPIClass
-  Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
-
-
-  void setup() {
-    display.setFont(&FreeSansOblique12pt7b);
-    display.begin();
-    display.setRotation(2);
-    display.fillScreen(BLACK);
-  }
-
-  void draw() {
-    display.setTextColor(YELLOW);
-    display.setCursor(10, 30);
-    display.print("hello ");
-    display.println("there");
-
-    display.drawRGBBitmap(40, 50, face_icon, 48, 48);
-  }
+  void setup();
+  void draw();
 }
