54084a37115445d03006f275444dac9f6239cf45
Gene Selkov <selkovjr@gmail.com>
2021-09-15 19:04:16 -0500

    hooked up with tc-menu

--- flow_controller/Display.cpp
+++ flow_controller/Display.cpp
@@ -1,11 +1,12 @@
 #include "Display.h"
 
-namespace Display {
-  // Software SPI mode (slow)
-  // Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
+// Software SPI mode (slow)
+// Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, CS_PIN, DC_PIN, MOSI_PIN, SCLK_PIN, RST_PIN);
 
-  // Hardware SPI using SPIClass
-  Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
+// Hardware SPI using SPIClass
+Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
+
+namespace Display {
 
   void setup() {
     display.setFont(&FreeSansOblique12pt7b);
--- flow_controller/flow_controller.ino
+++ flow_controller/flow_controller.ino
@@ -1,108 +1,50 @@
-#include "LibPrintf.h"
-#include "Fujikin.h"
+#include "flow_controller_menu.h"
+// default CPP main file for sketch
+#include <PlatformDetermination.h>
+
+#include <TaskManagerIO.h>
+
 #include "Display.h"
-#include "Trigger.h"
-#include "UserInput.h"
 
+#define SCREEN_WIDTH 128 //pixel width
+#define SCREEN_HEIGHT 128 //pixel height
 
-// the setup function runs once when you press reset or power the board
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(38400, SERIAL_8N1);  // SERIAL_8N1 is the default. It works with Fujikin.
+  // display.setFont(&FreeSansOblique12pt7b);
+  display.begin();
+  display.setRotation(2);
+  display.fillScreen(BLACK);
 
-  // initialize digital pin LED_BUILTIN as an output.
-  pinMode(LED_BUILTIN, OUTPUT);
+  setupMenu();
+}
 
-  Fujikin::setUpMax485();
-
-  Display::setup();
-  Trigger::setup();
-  UserInput::setup();
+void loop() {
+  taskManager.runLoop();
 }
 
 
-void loop() {
-  Display::draw();
+void CALLBACK_FUNCTION onModeChange(int id) {
+    // TODO - your menu change code
+}
 
-  digitalWrite(LED_BUILTIN, HIGH); // LED off
-  delay(5);
-  digitalWrite(LED_BUILTIN, LOW); // LED off
+void CALLBACK_FUNCTION onWarningLevelChange(int id) {
+    // TODO - your menu change code
+}
 
-  Fujikin::sendQuery("Product Name");
-  if (Fujikin::receivedResponse()) {
-    printf("product name: %s\n", Fujikin::recvStringBuf);
-  }
-  Fujikin::sendQuery("Manufacturer Model Number");
-  if (Fujikin::receivedResponse()) {
-    printf("model number: %s\n", Fujikin::recvStringBuf);
-  }
-  Fujikin::sendQuery("Gas Identifier");
-  if (Fujikin::receivedResponse()) {
-    printf("gas: %s\n", Fujikin::recvStringBuf);
-  }
-  Fujikin::sendQuery("nProcess Gases");
-  if (Fujikin::receivedResponse()) {
-    printf("gases known: %d\n", Fujikin::decodeUint8Buffer());
-  }
-  Fujikin::sendQuery("Gas CF");
-  if (Fujikin::receivedResponse()) {
-    printf("gas conversion factor: %f\n", Fujikin::decodeFloatBuffer());
-  }
+void CALLBACK_FUNCTION onCutOffLevelChange(int id) {
+    // TODO - your menu change code
+}
 
-  byte b[1] = {0x01};
-  Fujikin::sendCommand("Default Control Mode", b, 1);
-  if (Fujikin::receivedCommandAck()) {
-    printf("default control set to digital\n");
-  }
-  b[0] = {0x01};
-  Fujikin::sendCommand("Freeze Follow", b, 1);
-  if (Fujikin::receivedCommandAck()) {
-    printf("freeze follow disabled\n");
-  }
+void CALLBACK_FUNCTION onTalkEnableChange(int id) {
+    // TODO - your menu change code
+}
 
-  Fujikin::sendQuery("Freeze Follow");
-  if (Fujikin::receivedResponse()) {
-    printf("freeze follow: %x\n", Fujikin::decodeUint8Buffer());
-  }
+void CALLBACK_FUNCTION onTalkIntervalChange(int id) {
+    // TODO - your menu change code
+}
 
-//  byte w[2] = {0x00, 0x60};
-//  Fujikin::sendCommand("Setpoint", w, 2);
-//  if (Fujikin::receivedCommandAck()) {
-//    printf("setpoint sent\n");
-//  }
 
-  Fujikin::sendQuery("FM Status");
-  if (Fujikin::receivedResponse()) {
-    printf("flowmeter status: %d\n", Fujikin::decodeUint8Buffer());
-  }
-  Fujikin::sendQuery("FC Status");
-  if (Fujikin::receivedResponse()) {
-    printf("controller status: %d\n", Fujikin::decodeUint8Buffer());
-  }
-  Fujikin::sendQuery("Valve Status");
-  if (Fujikin::receivedResponse()) {
-    printf("valve status: %d\n", Fujikin::decodeUint8Buffer());
-  }
-  Fujikin::sendQuery("Valve Ramp Time");
-  if (Fujikin::receivedResponse()) {
-    printf("valve ramp time: %d\n", Fujikin::decodeUint32Buffer());
-  }
-  Fujikin::sendQuery("Setpoint");
-  if (Fujikin::receivedResponse()) {
-    printf("setpoint: %x\n", Fujikin::decodeUint16Buffer());
-  }
-  Fujikin::sendQuery("Filtered Setpoint");
-  if (Fujikin::receivedResponse()) {
-    printf("filtered setpoint: %x\n", Fujikin::decodeUint16Buffer());
-  }
-  Fujikin::sendQuery("Indicated Flow");
-  if (Fujikin::receivedResponse()) {
-    printf("indicated flow: %x\n", Fujikin::decodeUint16Buffer());
-  }
 
-  digitalWrite(13, digitalRead(2));
-  printf("Pin 2: %d\n", digitalRead(2));
-  printf("----\n");
-
-  delay(1000);
+void CALLBACK_FUNCTION onTriggerModeChange(int id) {
+    // TODO - your menu change code
 }
