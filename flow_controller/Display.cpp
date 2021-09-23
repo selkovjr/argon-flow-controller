a022b42c0204451dd344a472bfeb8905cba0bf72
Gene Selkov <selkovjr@gmail.com>
2021-09-23 02:15:08 -0500

    fully functional dial gauge; needs optimization

--- flow_controller/Display.cpp
+++ flow_controller/Display.cpp
@@ -11,73 +11,207 @@
 // Hardware SPI using SPIClass
 Adafruit_SSD1351 display = Adafruit_SSD1351(SCREEN_WIDTH, SCREEN_HEIGHT, &SPI, CS_PIN, DC_PIN, RST_PIN);
 
-#ifdef __arm__
-// should use uinstd.h to define sbrk but Due causes a conflict
-extern "C" char* sbrk(int incr);
-#else  // __ARM__
-extern char *__brkval;
-#endif  // __arm__
-
-int freeMemory() {
-  char top;
-#ifdef __arm__
-  return &top - reinterpret_cast<char*>(sbrk(0));
-#elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
-  return &top - __brkval;
-#else  // __arm__
-  return __brkval ? &top - __brkval : &top - __malloc_heap_start;
-#endif  // __arm__
-}
-
-bool is_blank[256];
-
 namespace Display {
   char buf[50];
+  char global_msg[50];
+  bool tile[256];
+  int16_t previous_value = -1;  // to ensure the needle is rendered the first time
+
   unsigned long count = 0;
 
-  void drawBackground ();
-
   void setup() {
+    memset(tile, 0, sizeof(tile));
     memset(buf, 0, sizeof(buf));
 
     // display.setFont(&FreeSansOblique12pt7b);  // tc-menu sets the font it likes
     display.begin();
     display.setRotation(2);
     drawBackground();
+    //draw_needle(0);  // Retrieve saved value here
   }
 
   void drawBackground () {
     display.fillScreen(BLACK);
     for (int i = 0; i < 256; i++) {
       if (sprite[i].index) {
-        display.drawRGBBitmap(sprite[i].x, sprite[i].y, sprite_bitmap[sprite[i].index], 8, 8);
+        display.drawRGBBitmap(
+          sprite[i].x,
+          sprite[i].y,
+          sprite_bitmap[sprite[i].index],
+          TILE_SIZE,
+          TILE_SIZE
+        );
       }
     }
   }
 
   void draw(char *msg) {
     display.setTextColor(BLACK);
-    display.setCursor(80, 2);
+    display.setCursor(0, 2);
     // display.print(count);
     // display.setCursor(0, 40);
     display.print(buf);
 
     display.setTextColor(YELLOW);
-    display.setCursor(80, 2);
+    display.setCursor(0, 2);
     // display.print(++count);
     // display.setCursor(0, 40);
 
     // sprintf(msg, "m: %d", freeMemory());
-    //display.print(msg);
+    display.print(msg);
     strcpy(buf, msg);
+
+    // display.print(global_msg);
+    // strcpy(buf, global_msg);
+
   }
 
   void draw_needle(int value) {
-    // outer radius = 43
-    int radius = 48;
-    float angle = (2 * 3.14159 / 360) * (120 + 300 * value / 80);
-    display.drawLine(64 + 11 * cos(angle), 64 + 11 * sin(angle), 64 + radius * cos(angle), 64 + radius * sin(angle), RED);
-    display.drawLine(64 + sin(angle) + 11 * cos(angle), 64 + cos(angle) + 11 * sin(angle), 64 + sin(angle) + radius * cos(angle), 64 + cos(angle) + radius * sin(angle), RED);
-    display.drawLine(64 - sin(angle) + 11 * cos(angle), 64 - cos(angle) + 11 * sin(angle), 64 - sin(angle) + radius * cos(angle), 64 - cos(angle) + radius * sin(angle), RED);
+
+    if (value == previous_value) {
+      return;
+    }
+
+    // The outer radius of the scale is 43
+    int outer_radius = 45;
+    int inner_radius = 11;
+
+    // The needle is rendered as three parallel lines
+    static uint8_t offest[3] = {-1, 0, 1};
+
+    // Not sure we want the point object. It may be wasteful
+    point start, end;
+
+
+    // The needle previously pointed at this angle
+    float angle = (2 * 3.14159 / 360) * (120 + 300 * previous_value / 80);  // 300: angular scale size; 80; number of steps
+
+    // reset sprite index
+    memset(tile, 0, sizeof(tile));
+
+    // Compute needle co-ordinates by imagining three horizontal lines drawn at
+    // 0° one pixel apart from each other and rotating their ends. We have to
+    // use three lines because the Adafruit graphics library offers no way to
+    // control line thickness. It does not transform primitives either, so
+    // we're doing our own rotation.
+    //
+    // For collision detection with supercover_line(), two lines spaced so that
+    // they encompass the three lines to be drawn are enough. These imaginary
+    // lines are extended 3px inward and are spaced 2px wider to cover for
+    // occasional false negatives due to rounding errors. This arrangement
+    // guarantees that the four central sprites are always redrawn.
+    //
+    start = rotate_point({CENTER_X + inner_radius - 3, CENTER_Y - 2}, angle);
+    end = rotate_point({CENTER_X + outer_radius, CENTER_Y - 2}, angle);
+    supercover_line(start.x, start.y, end.x, end.y);
+
+    start = rotate_point({CENTER_X + inner_radius - 3, CENTER_Y + 2}, angle);
+    end = rotate_point({CENTER_X + outer_radius, CENTER_Y + 2}, angle);
+    supercover_line(start.x, start.y, end.x, end.y);
+
+    // Re-render sprites touched by the needle in previous position. The ones
+    // needing update are marked by a boolean true value in the tile[] array.
+    //
+    for (int i = 0; i < 256; i++) {
+      if (tile[i]) {  // needs update
+        if (sprite[i].index) {  // i-th tile is a sprite
+          display.drawRGBBitmap(
+            sprite[i].x,
+            sprite[i].y,
+            sprite_bitmap[sprite[i].index],
+            TILE_SIZE,
+            TILE_SIZE
+          );
+        }
+        else {  // it is a blank tile
+          uint16_t x0 = (TILE_SIZE * i) % SCREEN_WIDTH;
+          uint16_t y0 = TILE_SIZE * (i / (SCREEN_HEIGHT / TILE_SIZE));
+          uint16_t x1 = x0 + TILE_SIZE - 1;
+          uint16_t y1 = y0 + TILE_SIZE - 1;
+          display.fillRect(x0, y0, TILE_SIZE, TILE_SIZE, BLACK);
+        }
+      }
+    }
+
+    // Save the current value and draw the needle.
+    previous_value = value;
+
+    // Now the needle points at this angle:
+    angle = (2 * 3.14159 / 360) * (120 + 300 * value / 80);  // 300: angular scale size; 80; number of steps
+
+    // Render the needle in current position using three parallel lines for thickness.
+    start = rotate_point({CENTER_X + inner_radius, CENTER_Y - 1}, angle);
+    end = rotate_point({CENTER_X + outer_radius, CENTER_Y - 1}, angle);
+    display.drawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, RED);
+
+    start = rotate_point({CENTER_X + inner_radius, CENTER_Y}, angle);
+    end = rotate_point({CENTER_X + outer_radius, CENTER_Y}, angle);
+    display.drawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, RED);
+
+    start = rotate_point({CENTER_X + inner_radius, CENTER_Y + 1}, angle);
+    end = rotate_point({CENTER_X + outer_radius, CENTER_Y + 1}, angle);
+    display.drawLine((int)start.x, (int)start.y, (int)end.x, (int)end.y, RED);
+  }
+
+  point rotate_point(point p, float angle) {
+    float s = sin(angle);
+    float c = cos(angle);
+
+    // translate point back to origin:
+    p.x -= CENTER_X;
+    p.y -= CENTER_Y;
+
+    // rotate point
+    float xnew = p.x * c - p.y * s;
+    float ynew = p.x * s + p.y * c;
+
+    // translate point back:
+    p.x = xnew + CENTER_X;
+    p.y = ynew + CENTER_Y;
+    return p;
+  }
+
+  // after https://www.redblobgames.com/grids/line-drawing.html
+  void supercover_line(uint8_t p0x, uint8_t p0y, uint8_t p1x, uint8_t p1y) {
+    int8_t dx = p1x - p0x, dy = p1y - p0y;
+    uint8_t nx = abs(dx), ny = abs(dy);
+    int8_t sign_x = dx > 0 ? 1 : -1, sign_y = dy > 0 ? 1 : -1;
+
+    // let p = new Point(p0.x, p0.y); -> (px, py)
+    // let points = [new Point(p.x, p.y)]; -> struct tile
+    uint8_t px = p0x, py = p0y;
+    for (uint8_t ix = 0, iy = 0; ix < nx || iy < ny;) {
+      int16_t decision = (1 + 2 * ix) * ny - (1 + 2 * iy) * nx;
+      if (decision == 0) {
+        // next step is diagonal
+        px += sign_x;
+        py += sign_y;
+        ix++;
+        iy++;
+      }
+      else if (decision < 0) {
+        // next step is horizontal
+        px += sign_x;
+        ix++;
+      }
+      else {
+        // next step is vertical
+        py += sign_y;
+        iy++;
+      }
+      //points.push(new Point(p.x, p.y));
+      tile[point_to_tile(px, py)] = true;
+    }
+  }
+
+  uint8_t pixel_to_tile(uint8_t p) {
+    return (uint8_t) (p / TILE_SIZE);
+  }
+
+  // Ordinal number of the tile containing the point
+  uint8_t point_to_tile(uint8_t x, uint8_t y) {
+    uint8_t cell_x = (uint8_t) (x / TILE_SIZE);
+    uint8_t cell_y = (uint8_t) (y / TILE_SIZE);
+    return cell_y * (SCREEN_WIDTH / TILE_SIZE) + cell_x;
   }
 }
--- flow_controller/Display.h
+++ flow_controller/Display.h
@@ -5,11 +5,28 @@
 #include "Adafruit_SSD1351.h" // Includes hardware specific library
 #include <Fonts/FreeSansOblique12pt7b.h>
 
-
 #include "dial_face.h"
 
-#define SCREEN_WIDTH 128 //pixel width
-#define SCREEN_HEIGHT 128 //pixel height
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 128
+
+#ifdef __arm__
+// should use uinstd.h to define sbrk but Due causes a conflict
+extern "C" char* sbrk(int incr);
+#else  // __ARM__
+extern char *__brkval;
+#endif  // __arm__
+
+// int freeMemory() {
+//   char top;
+// #ifdef __arm__
+//   return &top - reinterpret_cast<char*>(sbrk(0));
+// #elif defined(CORE_TEENSY) || (ARDUINO > 103 && ARDUINO != 151)
+//   return &top - __brkval;
+// #else  // __arm__
+//   return __brkval ? &top - __brkval : &top - __malloc_heap_start;
+// #endif  // __arm__
+// }
 
 // The only pin assignments that work with hardware SPI on a Mega.
 // Other, more intuitive assignments based on pin labels work in the
@@ -34,8 +51,19 @@
 
 
 namespace Display {
+  #define CENTER_X 64.5
+  #define CENTER_Y 64.5
+  #define TILE_SIZE 8
+
+  struct point {float x; float y;};
+  struct tile_coord {int x; int y;};
+
   void setup();
   void draw(char *);
+  point rotate_point(point, float);
+  uint8_t point_to_tile(uint8_t, uint8_t);
+  uint8_t pixel_to_tile(uint8_t);
+  void supercover_line(uint8_t, uint8_t, uint8_t, uint8_t);
   void draw_needle(int);
   void drawBackground();
 }
